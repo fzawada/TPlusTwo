9408da4278727783fe4b15ef0ab3437f7e8fabb0
Filip Zawada <contact@fzawada.com>
2025-11-05 18:32:01 +0000

    Added tests for ASP model binding/validation of required props

--- TPlusTwo.AspIntegrationTests/DtoBindingTests.cs
+++ TPlusTwo.AspIntegrationTests/DtoBindingTests.cs
@@ -14,7 +14,7 @@ public class DtoBindingTests
     public void OneTimeSetup()
     {
         _factory = new WebApplicationFactory<Program>()
-            .WithWebHostBuilder(builder => builder.UseUrls("http://localhost:7777"));
+            .WithWebHostBuilder(builder => builder.ConfigureServices(x => builder.UseUrls("http://localhost:7777")));
         _client = _factory.CreateClient();
     }
 
@@ -45,7 +45,7 @@ public class DtoBindingTests
     }
 
     [Test]
-    public async Task CreateRepoTrade_WithMissingRequiredMember_Returns400_AndErrorMessage()
+    public async Task CreateRepoTrade_WithMissingRequiredValueTypeMember_Returns400_AndErrorMessage()
     {
         var serializedPayloadWithMissingNominal = """
         {
@@ -65,15 +65,67 @@ public class DtoBindingTests
     }
 
     [Test]
-    public async Task CreateRepoTradet_WithNullForNonNullableRefType_Returns400()
+    public async Task CreateRepoTrade_WithMissingRequiredRefTypeMember_Returns400_AndErrorMessage()
     {
-        Assert.Inconclusive();
+        var serializedPayloadWithMissingInstrument = """
+        {
+          "tradeDate": "2025-11-05",
+          "settlementDate": "2025-11-05",
+          "nominal": 100
+        }
+        """;
+        var resp = await PostCreateRepoTrade(serializedPayloadWithMissingInstrument);
+
+        Assert.That(resp.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+        var p = await resp.Content.ReadAsStringAsync();
+        var problem = await resp.Content.ReadFromJsonAsync<ValidationProblemDetails>();
+        Assert.That(problem, Is.Not.Null);
+        Assert.That(problem!.Errors["$"], Has.Some.Contains("instrument"));
+        Assert.That(problem!.Errors["$"], Has.Some.Contains("missing required"));
     }
 
     [Test]
-    public async Task CreateRepoTradet_WithNullForNonNullableValueType_Returns400()
+    public async Task CreateRepoTradet_WithNullForRequiredNonNullableRefType_Returns400()
     {
-        Assert.Inconclusive();
+        var serializedPayloadWithNullInstrument = """
+        {
+          "tradeDate": "2025-11-05",
+          "settlementDate": "2025-11-05",
+          "nominal": 100,
+          "instrument": null
+        }
+        """;
+        var resp = await PostCreateRepoTrade(serializedPayloadWithNullInstrument);
+
+        Assert.That(resp.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+        var p = await resp.Content.ReadAsStringAsync();
+        var problem = await resp.Content.ReadFromJsonAsync<ValidationProblemDetails>();
+        Assert.That(problem, Is.Not.Null);
+        Assert.That(problem!.Errors, Contains.Key("Instrument"));
+        Assert.That(problem!.Errors["Instrument"], Has.Some.Contains("The Instrument field is required"));
+    }
+
+    [Test]
+    public async Task CreateRepoTradet_WithNullForRequiredNonNullableValueType_Returns400()
+    {
+        var serializedPayloadWithNullNominal = """
+        {
+          "tradeDate": "2025-11-05",
+          "settlementDate": "2025-11-05",
+          "nominal": null,
+          "instrument": "instrument1"
+        }
+        """;
+        var resp = await PostCreateRepoTrade(serializedPayloadWithNullNominal);
+
+        Assert.That(resp.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+        var p = await resp.Content.ReadAsStringAsync();
+        var problem = await resp.Content.ReadFromJsonAsync<ValidationProblemDetails>();
+        Assert.That(problem, Is.Not.Null);
+        Assert.That(problem!.Errors, Contains.Key("$.nominal"));
+        Assert.That(
+            problem!.Errors["$.nominal"],
+            Has.Some.Contains("The JSON value could not be converted to System.Decimal. Path: $.nominal"));
     }
 
     //TODO: test that Vogen validation plays well with ASP.NET Core model binding
